b8b7d2c219002bc2f174fe974b7c59c5bf1c6ee7
the-groot <sangwoo0795@naver.com>
2023-03-25 00:48:00 +0900

    upload

--- src/frontend/WearOs-for-Sound-/app/src/main/java/com/cbnu_voice/cbnu_imy/view/chatFragment.kt
+++ src/frontend/WearOs-for-Sound-/app/src/main/java/com/cbnu_voice/cbnu_imy/view/chatFragment.kt
@@ -1,9 +1,6 @@
 package com.cbnu_voice.cbnu_imy.view
 
-import android.media.SoundPool
-import android.os.Bundle
-import android.speech.SpeechRecognizer
-import android.speech.tts.TextToSpeech
+
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,6 +8,39 @@ import android.view.ViewGroup
 import com.cbnu_voice.cbnu_imy.Data.Message
 import com.cbnu_voice.cbnu_imy.Dto.CorpusDto
 import com.cbnu_voice.cbnu_imy.R
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.media.AudioAttributes
+import android.media.SoundPool
+import android.net.Uri
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.speech.RecognitionListener
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
+import android.speech.tts.TextToSpeech
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.cbnu_voice.cbnu_imy.Api.RetrofitBuilder
+import com.cbnu_voice.cbnu_imy.MainActivity
+import com.cbnu_voice.cbnu_imy.Utils.Constants.OPEN_GOOGLE
+import com.cbnu_voice.cbnu_imy.Utils.Constants.OPEN_SEARCH
+import com.cbnu_voice.cbnu_imy.Utils.Constants.RECEIVE_ID
+import com.cbnu_voice.cbnu_imy.Utils.Constants.SEND_ID
+import com.cbnu_voice.cbnu_imy.Utils.Time
+import com.cbnu_voice.cbnu_imy.databinding.FragmentChatBinding
+import kotlinx.android.synthetic.main.fragment_chat.*
+import kotlinx.coroutines.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.util.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -37,17 +67,280 @@ class chatFragment : Fragment() {
     private lateinit var stage : String      //register로 부터 입력받은 우울증 단계
     private var chatresponse=""   //ai chatbot 답변
 
+
+
+    private var binding: FragmentChatBinding? = null
+
+
+//    override fun onAttach(context: Context) {
+//        super.onAttach(context)
+//
+//        // 2. Context를 액티비티로 형변환해서 할당
+//        mainActivity = context as MainActivity
+//    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        requestPermission()
+        setAlarm()
+       // recyclerView()
+       // clickEvents()
+
+        val random = (0..2).random()
+        if(stage.equals("refuse")){
+            customBotMessage("안녕! , 오늘 기분은 어때?")
+            GlobalScope.launch {
+                delay(1000)
+
+                withContext(Dispatchers.Main) {
+                    rv_messages.scrollToPosition(adapter.itemCount - 1)
+                    soundPool.play(RefuseSoundList!!.get(0),2f, 2f,0,0,1f)
+
+                }
+            }
+        }
+
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recyclerView()
+        clickEvents()
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        //_binding= FragmentChatBinding.inflate(inflater,container,false)
+
+        val fragmentBinding = FragmentChatBinding.inflate(inflater, container, false)
+        binding = fragmentBinding
+
+
+        return fragmentBinding.root
+
         return inflater.inflate(R.layout.fragment_chat, container, false)
+    }
+
+//    override fun onDestroyView() {
+//        super.onDestroyView()
+//        _binding = null
+//    }
+
+    private fun setAlarm() {
+        textToSpeech = TextToSpeech(requireContext(), TextToSpeech.OnInitListener {
+            if (it == TextToSpeech.SUCCESS) {
+                val result = textToSpeech!!.setLanguage(Locale.KOREAN)
+                if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                    Log.e("TTS","해당언어는 지원되지 않습니다.")
+                    return@OnInitListener
+                }
+            }
+        })
+    }
+
+    // 권한 설정 메소드
+    private fun requestPermission() {
+        // 버전 체크, 권한 허용했는지 체크
+        if (Build.VERSION.SDK_INT >= 23 &&
+            ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.RECORD_AUDIO)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(activity as MainActivity,
+                arrayOf(Manifest.permission.RECORD_AUDIO), 0)
+        }
+    }
+
+    // 리스너 설정
+    private val recognitionListener: RecognitionListener = object : RecognitionListener {
+        // 말하기 시작할 준비가되면 호출
+        override fun onReadyForSpeech(params: Bundle) {
+            //Toast.makeText(applicationContext, "음성인식 시작", Toast.LENGTH_SHORT).show()
+            Toast.makeText(requireContext(), "음성인식 시작", Toast.LENGTH_SHORT).show()
+            binding.tvState.text = "이제 말씀하세요!"
+        }
+        // 말하기 시작했을 때 호출
+        override fun onBeginningOfSpeech() {
+            binding.tvState.text = "잘 듣고 있어요."
+        }
+        // 입력받는 소리의 크기를 알려줌
+        override fun onRmsChanged(rmsdB: Float) {}
+        // 말을 시작하고 인식이 된 단어를 buffer에 담음
+        override fun onBufferReceived(buffer: ByteArray) {}
+        // 말하기를 중지하면 호출
+        override fun onEndOfSpeech() {
+            binding.tvState.text = "끝!"
+        }
+        // 오류 발생했을 때 호출
+        override fun onError(error: Int) {
+            val message = when (error) {
+                SpeechRecognizer.ERROR_AUDIO -> "오디오 에러"
+                SpeechRecognizer.ERROR_CLIENT -> "클라이언트 에러"
+                SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS -> "퍼미션 없음"
+                SpeechRecognizer.ERROR_NETWORK -> "네트워크 에러"
+                SpeechRecognizer.ERROR_NETWORK_TIMEOUT -> "네트웍 타임아웃"
+                SpeechRecognizer.ERROR_NO_MATCH -> "찾을 수 없음"
+                SpeechRecognizer.ERROR_RECOGNIZER_BUSY -> "RECOGNIZER 가 바쁨"
+                SpeechRecognizer.ERROR_SERVER -> "서버가 이상함"
+                SpeechRecognizer.ERROR_SPEECH_TIMEOUT -> "말하는 시간초과"
+                else -> "알 수 없는 오류임"
+            }
+            binding.tvState.text = "에러 발생: $message"
+        }
+        // 인식 결과가 준비되면 호출
+        override fun onResults(results: Bundle) {
+            // 말을 하면 ArrayList에 단어를 넣고 textView에 단어를 이어줌
+            val matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+            for (i in matches!!.indices) binding.etMessage.setText(matches[i])
+            sendMessage()
+
+        }
+        // 부분 인식 결과를 사용할 수 있을 때 호출
+        override fun onPartialResults(partialResults: Bundle) {}
+        // 향후 이벤트를 추가하기 위해 예약
+        override fun onEvent(eventType: Int, params: Bundle) {}
+    }
+
+    private fun clickEvents() {
+
+        //Send a message
+        btn_send.setOnClickListener {
+            sendMessage()
+        }
+
+        //Scroll back to correct position when user clicks on text view
+        et_message.setOnClickListener {
+            GlobalScope.launch {
+                delay(100)
+
+                withContext(Dispatchers.Main) {
+                    rv_messages.scrollToPosition(adapter.itemCount - 1)
+
+                }
+            }
+        }
+    }
+
+    private fun recyclerView() {
+        adapter = MessagingAdapter()
+        rv_messages.adapter = adapter
+        //rv_messages.layoutManager = LinearLayoutManager(applicationContext)
+        rv_messages.layoutManager = LinearLayoutManager(requireContext())
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        //In case there are messages, scroll to bottom when re-opening app
+        GlobalScope.launch {
+            delay(100)
+            withContext(Dispatchers.Main) {
+                rv_messages.scrollToPosition(adapter.itemCount - 1)
+            }
+        }
+    }
+
+    private fun sendMessage() {
+        val message = et_message.text.toString()
+        val timeStamp = Time.timeStamp()
+
+        if (message.isNotEmpty()) {
+            //Adds it to our local list
+            messagesList.add(Message(message, SEND_ID, timeStamp))
+            et_message.setText("")
+
+            adapter.insertMessage(Message(message, SEND_ID, timeStamp))
+            rv_messages.scrollToPosition(adapter.itemCount - 1)
+
+            botResponse(message)
+
+        }
+    }
+
+    private fun botResponse(message: String) {
+
+        val timeStamp = Time.timeStamp()
+
+        GlobalScope.launch {
+            //Fake response delay
+            delay(1000)
+            var response=""
+
+            withContext(Dispatchers.Main) {
+
+                //Gets the response(3 case)
+                Chatbotlist(message);
+                response=chatresponse;
+
+                //Adds it to our local list
+
+                //messagesList.add(Message(response, RECEIVE_ID, timeStamp))
+                messagesList.add(Message(chatresponse, RECEIVE_ID, timeStamp))
+
+                //Inserts our message into the adapter
+                adapter.insertMessage(Message(chatresponse, RECEIVE_ID, timeStamp))
+                //adapter.insertMessage(Message(response, RECEIVE_ID, timeStamp))
+                //Scrolls us to the position of the latest message
+                rv_messages.scrollToPosition(adapter.itemCount - 1)
+
+                //Starts Google
+                when (response) {
+                    OPEN_GOOGLE -> {
+                        val site = Intent(Intent.ACTION_VIEW)
+                        site.data = Uri.parse("https://www.google.com/")
+                        startActivity(site)
+                    }
+                    OPEN_SEARCH -> {
+                        val site = Intent(Intent.ACTION_VIEW)
+                        val searchTerm: String? = message.substringAfterLast("search")
+                        site.data = Uri.parse("https://www.google.com/search?&q=$searchTerm")
+                        startActivity(site)
+                    }
+
+                }
+
+                //textToSpeech?.speak(response, TextToSpeech.QUEUE_FLUSH, null)
+                textToSpeech?.playSilentUtterance(750,TextToSpeech.QUEUE_ADD,null) // deley시간 설정
+            }
+        }
+    }
+
+    private fun customBotMessage(message: String) {
+
+        GlobalScope.launch {
+            delay(1000)
+            withContext(Dispatchers.Main) {
+                val timeStamp = Time.timeStamp()
+                messagesList.add(Message(message, RECEIVE_ID, timeStamp))
+                adapter.insertMessage(Message(message, RECEIVE_ID, timeStamp))
+
+                rv_messages.scrollToPosition(adapter.itemCount - 1)
+
+                //textToSpeech?.speak(message, TextToSpeech.QUEUE_FLUSH, null)
+                //textToSpeech?.playSilentUtterance(750,TextToSpeech.QUEUE_ADD,null) // deley시간 설정
+            }
+        }
+    }
+
+
+
+    private suspend fun Chatbotlist(s: String) {
+        withContext(Dispatchers.IO) {
+
+            runCatching {
+                val retrofit = RetrofitBuilder.chatbotapi.getKogpt2Response(s)
+                val res = retrofit.execute().body()
+                //res.code() == 200
+                println("res = ${res}")
+                chatresponse= res!!.answer
+            }.getOrDefault(false)
+        }
+
     }
 
 }
