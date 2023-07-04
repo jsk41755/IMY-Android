9208de1f05148523b26fa08174b031075d3ea4ed
JeongSG <jsk41755@naver.com>
2023-07-05 01:07:03 +0900

    fragmentSet.kt 파일 외 4개 삭제

--- src/frontend/WearOs-for-Sound-/app/src/main/java/com/cbnu_voice/cbnu_imy/Api/RetrofitBuilder.kt
+++ src/frontend/WearOs-for-Sound-/app/src/main/java/com/cbnu_voice/cbnu_imy/Api/RetrofitBuilder.kt
@@ -7,25 +7,16 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 
 object RetrofitBuilder {
-    var userapi: UserApi
-    var corpusapi: CorpusApi
     var chatbotapi: ChatbotApi
     var pulseApi: PulseApi
 
-    val nestnetip= "http://113.198.137.188:81/"
-    val houseip="http://113.198.137.200:4511"
-    val localhost="http://10.0.2.2:5000"
-    val pulseApihost = "http://172.17.0.4:8083"
     val nova = BuildConfig.NOVA
     init{
-        println("connected")
         val retrofit= Retrofit.Builder()
             .baseUrl(nova)   //요청 보내는 API 서버 url /로 끝나야 함
             .addConverterFactory(ScalarsConverterFactory.create() )
             .addConverterFactory(GsonConverterFactory.create())//Gson을 역직렬화
             .build()
-        userapi=retrofit.create(UserApi::class.java)
-        corpusapi=retrofit.create(CorpusApi::class.java)
         chatbotapi=retrofit.create(ChatbotApi::class.java)
         pulseApi = retrofit.create(PulseApi::class.java)
 
